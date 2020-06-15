d3039b7d2ac281eeacec18e0ee451dda16df93c1
gianna-k <56313650+gianna-k@users.noreply.github.com>
2020-06-15 14:56:48 +0900

    0615 김지현

--- src/main/Main_Teacher_Controller.java
+++ src/main/Main_Teacher_Controller.java
@@ -3,8 +3,6 @@
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 import launch.AppMain;
 import util.Navigator;
@@ -17,25 +15,24 @@ public class Main_Teacher_Controller extends Main_Master_Controller implements I
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		super.initialize(location, resources);
-		
-		// 초기화면
+
+		// 초기화면(강의 화면, 강의서브메뉴)
 		Navigator.loadSubMenu(Navigator.TEACHER_CLASS_MENU);
 		Navigator.loadPages(Navigator.TEACHER_CLASS_LIST);
-//		Navigator.loadPages("/teacher/classes/teacher_main.fxml");
 
-		// 버튼 누르면 inner fxml 바꾸기
+		// 버튼 누르면 inner fxml, subMenu Change
+
+		// btn1 : 강의
 		btn1.setOnAction(e -> {
 			Navigator.loadSubMenu(Navigator.TEACHER_CLASS_MENU);
 			Navigator.loadPages(Navigator.TEACHER_CLASS_LIST);
 		});
-		//btn2.setOnAction(e -> Navigator.loadPages(Navigator.TASK_LIST));
-		btn2.setOnAction(new EventHandler<ActionEvent>() {
-			
-			@Override
-			public void handle(ActionEvent event) {
-				AppMain.app.getBasic().setClass_no(0);
-				Navigator.loadPages(Navigator.TEACHER_TASK_LIST);
-			}
+
+		// btn2: 과제
+		btn2.setOnAction(e -> {
+			AppMain.app.getBasic().setClass_no(0);
+			Navigator.loadSubMenu(Navigator.TEACHER_TASK_MENU);
+			Navigator.loadPages(Navigator.TEACHER_TASK_LIST);
 		});
 	}
 }
