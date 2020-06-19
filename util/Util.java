4bf8ce8ba1be75975358cb4c3e2f8010594dfad1
lismsdh2 <64739244+lismsdh2@users.noreply.github.com>
2020-06-20 04:30:02 +0900

    0620 도현호 - 첨부파일 용량 제한 기능 추가

--- util/Util.java
+++ util/Util.java
@@ -1,12 +1,18 @@
 ﻿package util;
 
+import java.io.File;
+import java.io.IOException;
+
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextInputControl;
+import javafx.stage.Popup;
+import javafx.stage.Window;
 
 public class Util {
 
@@ -91,19 +97,61 @@ public class Util {
 		});
 	}
 	
-	// textfield 미입력 시 warning label 보이기
-	public static void showWarningLabel(TextField txtfield, Label lbl) {
-
-		txtfield.textProperty().addListener(new ChangeListener<String>() {
-
-			@Override
-			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-				if (newValue.length() == 0) {
-					lbl.setVisible(true);
-				} else {
-					lbl.setVisible(false);
-				}
-			}
-		});
+	public static void waitPopup(String msg, Window window) {
+		
+		Popup popup = new Popup();
+		System.out.println("1");
+		try {
+//			Parent parent  = FXMLLoader.load(Parent.class.getResource("../fxml/util/waitAlert.fxml"));
+			Parent parent  = FXMLLoader.load(Util.class.getResource("../fxml/util/waitAlert.fxml"));
+			
+		System.out.println("2");
+			Label label = (Label) parent.lookup("#lblMessage");
+			label.setText(msg);
+		System.out.println(label.getText());
+		System.out.println("3");
+			popup.getContent().add(parent);
+			popup.setAutoHide(true);
+		System.out.println("4");
+			popup.show(window);
+		System.out.println("5");
+			
+		} catch (IOException e) {
+//			e.printStackTrace();
+			System.out.println("팝업생성 오류");
+		}
 	}
+	
+	public static String fileSizeCheck(File file) {
+	
+		long restrictSize = 1024 * 1024 * 10;				//10MB
+		long fileSize = file.length();
+		double size = 0.0;
+		
+		String sizeUnit = "";								//단위표기
+		String strSize = "";								//나고 난 뒤 값
+	
+		if(fileSize <= restrictSize ) {
+			
+			if(fileSize >= 1024*1024) {
+				sizeUnit = "MB";
+				size = ((double)fileSize/(double)(1024*1024));
+				strSize = String.format("%,.3f", size) + sizeUnit;
+				System.out.println(strSize);
+				
+			} else {
+				sizeUnit = "KB";
+				size = ((double)fileSize/(double)(1024));
+				strSize = String.format("%,.3f", size) + sizeUnit;
+				System.out.println(strSize);
+			}
+		} else {
+			
+			strSize = null;
+			Util.showAlert("", "10MB 이하만 등록 가능합니다.", AlertType.INFORMATION);
+		}
+		
+		return strSize;
+	}
+
 }
